1bb7542eda917a7c6849fb9e5b427bfdb2c5225d
karu9 <picheryc@gmail.com>
2018-12-08 02:12:48 +0100

    days'

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -5,23 +5,34 @@ for i in lines :
     y = int(i.split(" @ ")[1].split(": ")[0].split(",")[1])
     deltax = int(i.split(" @ ")[1].split(": ")[1].split("x")[0])
     deltay = int(i.split(" @ ")[1].split(": ")[1].split("x")[1])
-    coords.append([x,x+deltax,y,y+deltay])
-minX = min(list(map(lambda x : x[0], coords)))
-maxX = max(list(map(lambda x : x[1], coords)))
-minY = min(list(map(lambda x : x[2], coords)))
-maxY = max(list(map(lambda x : x[3], coords)))
-print(maxX)
-print(maxY)
-
-count = 0
-for i in range(minX, maxX+1):
+    coords.append([x,x+deltax-1,y,y+deltay-1])
+overlaps = []
+for i in range(len(coords)):
     print(i)
-    for j in range(minY, maxY+1):
-        l = 0
-        for coord in coords :
-            if coord[0] <= i and coord[1] >= i and coord[2] <= j and coord[3] >= j:
-                l = l + 1
-            if l > 1 :
-                count = count + 1
-                break
-print(count)
+    for j in range(i+1, len(coords)):
+        overlapX = list(filter(lambda x : x in range(coords[j][0], coords[j][1]+1), range(coords[i][0], coords[i][1]+1)))
+        if len(overlapX) == 0:
+            continue
+        overlapY = list(filter(lambda y : y in range(coords[j][2], coords[j][3]+1), range(coords[i][2], coords[i][3]+1)))
+        if len(overlapY) == 0:
+            continue
+        for x in overlapX:
+            for y in overlapY:
+                co = str(x) + "," + str(y)
+                overlaps.append(co)
+print(len(set(overlaps)))
+def overlap(coord, overlaps):
+    for i in range(coord[0], coord[1]+1):
+        for j in range(coord[2], coord[3]+1):
+            co = str(i) + "," + str(j)
+            if co in overlaps:
+                return True
+    return False
+            
+for i in range(len(coords)):
+    print(i)
+    if not overlap(coords[i], overlaps):
+        print("result ", i+1)
+        break
+    
+            
--- src/day4/day4.py
+++ src/day4/day4.py
@@ -1,26 +1,46 @@
 lines = open('input.txt').readlines()
-valid1 = 0
-valid2 = 0
-def validate(words):
-    for i in range(len(words)):
-     for j in range(i + 1, len(words)):
-         if words[i] == words[j]:
-             return False
-    return True
-def validate2(words):
-    for i in range(len(words)):
-     for j in range(i + 1, len(words)):
-         wordi = sorted(list(map(lambda x : x, words[i])))
-         wordj = sorted(list(map(lambda x : x, words[j])))
-         if wordi == wordj:
-             return False
-    return True
+# link day with guard number #
+guards = sorted(list(map(lambda x : [x[3][1:], x[0][1:]], list(filter(lambda y: y[2] == "Guard", [x.split(" ") for x in lines])))))
+# fill with sleep / up time #
+for guard in guards:
+    day = guard[1]
+    times = []
+    for line in lines :
+        l = line.split(" ")
+        if l[0][1:] == day:
+            if l[2] == "Guard":
+                continue
+            elif l[2] == "falls":
+                times.append([int(l[1][3:-1]), False])
+            else :
+                times.append([int(l[1][3:-1]), True])
+    times = sorted(times)
+    if len(times) == 0 or not times[0][0] == 0:
+        times = [[0, 1]] + times
+    minutesAsleep = []
+    status = True
+    for i in range(0,60):
+        for time in times:
+            if i == time[0]:
+                status = time[1]
+                break
+        if not status :
+            minutesAsleep.append(i)
+    guard.append(minutesAsleep)
+guardIds = list(set(map(lambda x : x[0], guards)))
+minutes = []
+for guardId in guardIds:
+    minu = []
+    for minute in list(map(lambda y: y[2], filter(lambda x: x[0] == guardId, guards))):
+        minu = minu + minute
+    minutes.append(minu)
+ms = [len(x) for x in minutes].index(max([len(x) for x in minutes]))
+mst = minutes[ms]
+print(guardIds)
+print(guardIds[ms])
+mostSlept = mst[[mst.count(x) for x in mst].index(max([mst.count(x) for x in mst]))]
+print(mostSlept)
+print(int(guardIds[ms]) * mostSlept)
+        
+
     
-for line in lines:
-    words = line[:-1].split(' ')
-    if validate(words):
-        valid1+= 1
-    if validate2(words):
-        valid2+= 1
-print(valid1)
-print(valid2)
